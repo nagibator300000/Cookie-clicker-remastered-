70538c11b3b2883a2a68217617b3db37bfdaa89d
Dmitriy-Bukreev <dmitriy.bukreev.job@gmail.com>
2024-12-14 19:57:15 +0300

    Installed tsx

--- backend/build/backend/utils.js
+++
@@ -1,7 +0,0 @@
-export function loadEnv(key) {
-    const val = process.env[key];
-    if (!val) {
-        throw new Error(`Cant load var ${key}`);
-    }
-    return val;
-}
--- backend/server.ts
+++ backend/server.ts
@@ -5,7 +5,7 @@ import corsPlugin from "cors";
 import { PrismaClient } from "@prisma/client";
 import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 import { Strategy } from "passport-google-oauth20";
-import { loadEnv } from "./utils.js";
+import { loadEnv } from "./utils";
 import passport from "passport";
 import bodyParser from "body-parser";
 import GameStatsSchema from "../schemas/gameStats";
@@ -65,13 +65,6 @@ declare module "express-session" {
 
 const app = express();
 
-type Stat = { id: number; money: number };
-const stats: Stat[] = [
-  { id: 0, money: 0 },
-  { id: 1, money: 10 },
-  { id: 2, money: 50 },
-];
-
 app.use(cookieMiddleware);
 app.use(sessionMiddleware);
 app.use(bodyParser.json());
@@ -108,15 +101,6 @@ passport.deserializeUser(async (userData, done) => {
 
 app.get("/", (_req, res) => {
   res.send("Nuke");
-});
-
-app.get("/stat/:userId", (req, res) => {
-  console.log("aaa");
-  const stat = stats[Number(req.params.userId)];
-  if (!stat) {
-    res.status(404).send("Not found");
-  }
-  res.json(stat);
 });
 
 app.get(
