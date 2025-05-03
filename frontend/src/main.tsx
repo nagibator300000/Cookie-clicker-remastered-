b5b04dbd92996aaeceaf7acb47ffa3123a440e55
nagibator300000 <raul.tatulyan@gmail.com>
2025-05-03 20:23:45 +0300

    added favicon

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './index.scss'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { FetchError } from './utils/fetchJSON.ts'
 import { InventoryProvider } from './components'
+import { Helmet } from "react-helmet"
 
 const client = new QueryClient({
   defaultOptions: {
@@ -22,6 +23,14 @@ const client = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Helmet>
+      <link rel="icon" type="image/png" href={`${import.meta.env.VITE_FRONT_URL}/favicon-96x96.png`} sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href={`${import.meta.env.VITE_FRONT_URL}/favicon.svg`} />
+      <link rel="shortcut icon" href={`${import.meta.env.VITE_FRONT_URL}/favicon.ico`} />
+      <link rel="apple-touch-icon" sizes="180x180" href={`${import.meta.env.VITE_FRONT_URL}/apple-touch-icon.png`} />
+      <meta name="apple-mobile-web-app-title" content="Hollow clicker" />
+      <link rel="manifest" href={`${import.meta.env.VITE_FRONT_URL}/site.webmanifest`} />
+    </Helmet>
     <div className="overlayer">
       <div className="topFleur"></div>
       <div className="bottomFleur"></div>
